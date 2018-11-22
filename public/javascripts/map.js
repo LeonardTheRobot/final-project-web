816388aecd9823e6df2ea1a8b0a8db5abf812a50
Sam Chatfield <samchatfield97@gmail.com>
2018-11-22 21:57:22 +0000

    Start drawing map. Add a PNG version of the map as a static resource. Place the map as a static image with a transparent canvas over it in HTML. This will allow us to draw and update the robot's location overlayed on the map without having to redraw the map each time. Create JavaScript to pull the robot data from the api every 10 seconds and draw the robot's location if required. The update takes into account whether the robot has moved; if not, it won't redraw for efficiency.

---
+++ public/javascripts/map.js
@@ -0,0 +1,32 @@
+const mapCanvas = $('#map-canvas').get(0);
+const mapContext = mapCanvas.getContext('2d');
+
+mapContext.fillStyle = 'red';
+const robotSize = 10;
+let lastLocation;
+
+function drawMap() {
+  $.get('/api/robot', (robotData) => {
+    if (robotData.location) {
+      const rx = robotData.location.x;
+      const ry = robotData.location.y;
+      if (lastLocation && rx === lastLocation.x && ry === lastLocation.y) {
+        console.log('Skip draw (no change)');
+      } else {
+        console.log('Draw');
+        const px = (robotData.location.x / 0.05) - (robotSize / 2);
+        const py = (robotData.location.y / 0.05) - (robotSize / 2);
+        mapContext.clearRect(0, 0, mapCanvas.width, mapCanvas.height);
+        mapContext.fillRect(px, py, robotSize, robotSize);
+        lastLocation = robotData.location;
+      }
+    } else {
+      console.log('NO ROBOT LOCATION');
+    }
+  });
+}
+
+drawMap();
+setInterval(() => {
+  requestAnimationFrame(drawMap);
+}, 10000);
