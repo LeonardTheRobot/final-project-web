132646a3be9f68553057b0b0f542053a0def0308
Sam Chatfield <samchatfield97@gmail.com>
2018-11-26 17:44:18 +0000

    Create function to update the order queue from the data from /api/robot

--- public/javascripts/systemStatus.js
+++ public/javascripts/systemStatus.js
@@ -16,37 +16,62 @@ function coordsToPixels(coordObj) {
   };
 }
 
-function updateMap(robotData) {
-  if (robotData.location) {
-    const rx = robotData.location.x;
-    const ry = robotData.location.y;
+function updateMap(robotLocation) {
+  if (robotLocation) {
+    const rx = robotLocation.x;
+    const ry = robotLocation.y;
     if (lastLocation && rx === lastLocation.x && ry === lastLocation.y) {
       console.log('Skip draw (no change)');
     } else {
       console.log('Draw');
-      const pixelsObj = coordsToPixels(robotData.location);
+      const pixelsObj = coordsToPixels(robotLocation);
       mapContext.clearRect(0, 0, mapCanvas.width, mapCanvas.height);
       mapContext.fillRect(pixelsObj.x, pixelsObj.y, robotSize, robotSize);
-      lastLocation = robotData.location;
+      lastLocation = robotLocation;
     }
   } else {
     console.log('NO ROBOT LOCATION');
   }
 }
 
+function updateOrderQueue(robotOrderQueue) {
+  console.log(`Order Queue:\n${JSON.stringify(robotOrderQueue)}`);
+  robotOrderQueue.forEach((orderId) => {
+    $.get(`/api/orders/${orderId}`, (orderData) => {
+      console.log(`Order Data ${orderId}:\n${JSON.stringify(orderData)}`);
+      const itemsList = $('<ul>');
+      orderData.items.forEach((item) => {
+        itemsList.append($('<li>').text(item));
+      });
+      // const itemsList = $('<ul>').append(
+      //   $('<li>').text()
+      // );
+      const tableRow = $('<tr>').append(
+        $('<td>').text(orderId),
+        $('<td>').text(orderData.user),
+        $('<td>').text(orderData.zone),
+        $('<td>').append(itemsList),
+        $('<td>').text(orderData.status),
+      ).appendTo('#order-queue-table-body');
+      console.log(tableRow);
+    });
+  });
+}
+
 function update() {
   $.get('/api/robot', (robotData) => {
     requestAnimationFrame(() => {
-      updateMap(robotData);
+      updateMap(robotData.location);
+      updateOrderQueue(robotData.orderQueue);
     });
   });
 }
 
 update();
 setInterval(() => {
-  if ($('#auto-update-checkbox').is(':checked')) {
+  if (lastLocation && $('#auto-update-checkbox').is(':checked')) {
     update();
   } else {
     console.log('Auto-update not checked');
   }
-}, 1000);
+}, 10000);
