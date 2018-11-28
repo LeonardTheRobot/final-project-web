613cc6b50e886b0afa5141e591d1dfd4ce41827b
Sam Chatfield <samchatfield97@gmail.com>
2018-11-28 23:57:18 +0000

    Implement form submission. Take the user's inputted values, transform them to the form accepted by orders API and POST. Refactor placeOrder function calls.

--- public/javascripts/placeOrder.js
+++ public/javascripts/placeOrder.js
@@ -1,16 +1,13 @@
-function populateUsers() {
-  $.get('/api/users', (users) => {
-    users.forEach((user) => {
-      $('#user-select').append($('<option>', {
-        value: user._id,
-        text: user.name,
-      }));
-    });
+function populateUsers(users) {
+  users.forEach((user) => {
+    $('#user-select').append($('<option>', {
+      value: user.name,
+      text: user.name,
+    }));
   });
 }
 
-function populateZones() {
-  const zones = ['A', 'B', 'C', 'D'];
+function populateZones(zones) {
   zones.forEach((zone) => {
     $('#zone-select').append($('<option>', {
       value: zone,
@@ -19,42 +16,76 @@ function populateZones() {
   });
 }
 
-function populateItems() {
-  $.get('/api/items', (items) => {
-    const itemsCardBody = $('#items-card > .card-body');
+function populateItems(items) {
+  const itemsCardBody = $('#items-card > .card-body');
+  items.forEach((item) => {
+    const itemRow = $('<div>', {
+      class: 'form-group row',
+    });
+    itemRow.append($('<label>', {
+      class: 'col-form-label col-lg-2',
+      text: item.name,
+    }));
+    const itemFieldDiv = $('<div>', {
+      class: 'col-lg-2',
+    });
+    itemFieldDiv.append($('<input>', {
+      class: 'form-control',
+      id: `${item.name.toLowerCase()}-field`,
+      name: item.name,
+      type: 'number',
+      min: 0,
+      max: 5,
+      value: 0,
+    }));
+    itemRow.append(itemFieldDiv);
+    itemsCardBody.append(itemRow);
+  });
+}
+
+function bindFormSubmit(items) {
+  $('#order-form').submit((e) => {
+    e.preventDefault();
+
+    const itemList = [];
     items.forEach((item) => {
-      const itemRow = $('<div>', {
-        class: 'form-group row',
-      });
-      itemRow.append($('<label>', {
-        class: 'col-form-label col-lg-2',
-        text: item.name,
-      }));
-      const itemFieldDiv = $('<div>', {
-        class: 'col-lg-2',
-      });
-      itemFieldDiv.append($('<input>', {
-        class: 'form-control',
-        name: item.name,
-        type: 'number',
-        min: 0,
-        max: 5,
-        value: 0,
-      }));
-      itemRow.append(itemFieldDiv);
-      itemsCardBody.append(itemRow);
+      const itemQuantity = $(`#${item.name.toLowerCase()}-field`).val();
+      for (let i = 0; i < itemQuantity; i += 1) {
+        itemList.push(item.name);
+      }
+    });
+
+    const orderData = {
+      user: $('#user-select').val(),
+      zone: $('#zone-select').val(),
+      items: itemList,
+    };
+
+    $.post({
+      url: '/api/orders',
+      data: JSON.stringify(orderData),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      success: (res) => {
+        // If order is placed successfully, redirect user to view it
+        // TODO: redirect to view for order once implemented
+        window.location.replace(`/api/orders/${res._id}`);
+      },
     });
   });
 }
 
-function bindFormSubmit() {
-  $('#order-form').submit((e) => {
-    e.preventDefault();
-    console.log(JSON.stringify($('#order-form').serializeArray()));
-  });
-}
+// Get all users from the API and populate the user select
+$.get('/api/users', (users) => {
+  populateUsers(users);
+});
 
-populateUsers();
-populateZones();
-populateItems();
-bindFormSubmit();
+// Populate the zone select
+const zones = ['A', 'B', 'C', 'D'];
+populateZones(zones);
+
+// Get all items from the API and populate the list of items
+$.get('/api/items', (items) => {
+  populateItems(items);
+  bindFormSubmit(items);
+});
