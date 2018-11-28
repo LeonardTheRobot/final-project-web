7d7bf8177ec8d0e52ce94ada468214bb5bd06285
Sam Chatfield <samchatfield97@gmail.com>
2018-11-28 22:30:24 +0000

    Populate items select from api. Refactor placeOrder.js adding functions.

--- public/javascripts/placeOrder.js
+++ public/javascripts/placeOrder.js
@@ -1,20 +1,60 @@
-$('#order-form').submit((e) => {
-  e.preventDefault();
-});
+function populateUsers() {
+  $.get('/api/users', (users) => {
+    users.forEach((user) => {
+      $('#user-select').append($('<option>', {
+        value: user._id,
+        text: user.name,
+      }));
+    });
+  });
+}
 
-$.get('/api/users', (users) => {
-  users.forEach((user) => {
-    $('#user-select').append($('<option>', {
-      value: user._id,
-      text: user.name,
+function populateZones() {
+  const zones = ['A', 'B', 'C', 'D'];
+  zones.forEach((zone) => {
+    $('#zone-select').append($('<option>', {
+      value: zone,
+      text: zone,
     }));
   });
-});
+}
 
-const zones = ['A', 'B', 'C', 'D'];
-zones.forEach((zone) => {
-  $('#zone-select').append($('<option>', {
-    value: zone,
-    text: zone,
-  }));
-});
+function populateItems() {
+  $.get('/api/items', (items) => {
+    const itemsCardBody = $('#items-card > .card-body');
+    items.forEach((item) => {
+      const itemRow = $('<div>', {
+        class: 'form-group row',
+      });
+      itemRow.append($('<label>', {
+        class: 'col-form-label col-lg-2',
+        text: item.name,
+      }));
+      const itemFieldDiv = $('<div>', {
+        class: 'col-lg-2',
+      });
+      itemFieldDiv.append($('<input>', {
+        class: 'form-control',
+        name: item.name,
+        type: 'number',
+        min: 0,
+        max: 5,
+        value: 0,
+      }));
+      itemRow.append(itemFieldDiv);
+      itemsCardBody.append(itemRow);
+    });
+  });
+}
+
+function bindFormSubmit() {
+  $('#order-form').submit((e) => {
+    e.preventDefault();
+    console.log(JSON.stringify($('#order-form').serializeArray()));
+  });
+}
+
+populateUsers();
+populateZones();
+populateItems();
+bindFormSubmit();
