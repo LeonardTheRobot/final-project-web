40638283bfc4487f3c12e1657c2afa7615a3d99f
Sam Chatfield <samchatfield97@gmail.com>
2018-12-06 10:39:05 +0000

    Implement the view all orders page. Display a table sorted by order status where current orders show at the top. Have links from order IDs to their order pages. Colour code the orders based on their statuses.

---
+++ public/javascripts/viewAllOrders.js
@@ -0,0 +1,71 @@
+// Priority values for order statuses
+const statusSortValues = {
+  COLLECTION: 0,
+  IN_PROGRESS: 1,
+  PENDING: 2,
+  COMPLETED: 3,
+  FAILED: 4,
+};
+
+// Text colour status mapping
+const statusColourClassMapping = {
+  COLLECTION: 'text-warning',
+  IN_PROGRESS: 'text-info',
+  PENDING: null,
+  COMPLETED: 'text-success',
+  FAILED: 'text-danger',
+};
+
+/**
+ * Array.prototype.sort comparator which compares two orders by their status' priority, defined in
+ * statusSortValues
+ */
+function compareOrdersByStatus(a, b) {
+  let sortVal;
+  if (statusSortValues[a.status] < statusSortValues[b.status]) {
+    sortVal = -1;
+  } else if (statusSortValues[a.status] > statusSortValues[b.status]) {
+    sortVal = 1;
+  } else {
+    sortVal = 0;
+  }
+  return sortVal;
+}
+
+function populateOrderTable(orders) {
+  const sortedOrders = orders.sort(compareOrdersByStatus);
+  sortedOrders.forEach((order) => {
+    // Create a link to the view order page for each order
+    const idLink = $('<a>', {
+      href: `/order/view/${order._id}`,
+      text: order._id,
+    });
+
+    // Create a bullet point list of the items in this order
+    const itemsList = $('<ul>');
+    order.items.forEach((item) => {
+      itemsList.append($('<li>').text(item));
+    });
+
+    // Create a new table row from the data for this order
+    const row = $('<tr>').append(
+      $('<td>').append(idLink),
+      $('<td>').text(order.user),
+      $('<td>').text(order.zone),
+      $('<td>').append(itemsList),
+      $('<td>').text(order.status),
+    );
+
+    if (statusColourClassMapping[order.status]) {
+      row.addClass(statusColourClassMapping[order.status]);
+    }
+
+    $('#order-table-body').append(row);
+  });
+}
+
+$(document).ready(() => {
+  $.get('/api/orders', (orderData) => {
+    populateOrderTable(orderData);
+  });
+});
